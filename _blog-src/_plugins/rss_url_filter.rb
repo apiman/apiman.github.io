028b0d557392c0cdb54c29261fa18f8c98962ab7
Marc Savy <marc@rhymewithgravy.com>
2015-06-17 11:28:20 +0100

    Test switching to absolute urls by using liquid filter on xml feed

---
+++ _blog-src/_plugins/rss_url_filter.rb
@@ -0,0 +1,14 @@
+module Jekyll
+  module RSSURLFilter
+    def relative_urls_to_absolute( input )
+      url = "http://www.apiman.io"
+      input\
+        .gsub('src="/', 'src="' + url + '/')\
+        .gsub('href="/', 'href="' + url + '/')\
+        .gsub('src=&quot;/', 'src=&quot;' + url + '/')\
+        .gsub('href=&quot;/', 'href=&quot;' + url + '/')
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::RSSURLFilter)
