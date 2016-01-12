c631f8e357342fac62dbe2dccb6d5da0cf0f0c45
Marc Savy <marc@rhymewithgravy.com>
2016-01-12 20:30:38 +0000

    Less terrible name for filter plugin

--- _blog-src/_plugins/find_post_by_path.rb
+++ _blog-src/_plugins/find_post_by_path.rb
@@ -1,8 +1,8 @@
 require 'uri'
 
 module Jekyll
-  module FindPostByNameFilter
-    def find_post_by_name( input )
+  module FindPostByPathFilter
+    def find_post_by_path( input )
       url = URI(input)
       extname = File.extname(url.path) || ""
       url.path.split("/").last.chomp(extname)
@@ -10,4 +10,4 @@ module Jekyll
   end
 end
 
-Liquid::Template.register_filter(Jekyll::FindPostByNameFilter)
+Liquid::Template.register_filter(Jekyll::FindPostByPathFilter)
