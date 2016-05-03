b61d9ff5d6c876f4fd5932d848527acd73bfc48d
Marc Savy <marc@rhymewithgravy.com>
2016-05-03 13:53:31 +0100

    fix(blog) Get rid of extraneous white spaces caused by liquid

---
+++ _blog-src/_plugins/strip.rb
@@ -0,0 +1,15 @@
+#https://github.com/aucor/jekyll-plugins/blob/master/LICENCE
+# Replaces multiple newlines and whitespace 
+# between them with one newline
+
+module Jekyll
+  class StripTag < Liquid::Block
+
+    def render(context)
+      super.gsub /\n\s*\n/, "\n"
+    end
+
+  end
+end
+
+Liquid::Template.register_tag('strip', Jekyll::StripTag)
